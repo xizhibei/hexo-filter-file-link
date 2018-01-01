af6a93cec448aee14b924722145845255c147827
Xu Zhipei <xuzhipei@gmail.com>
2018-01-01 21:58:12 +0800

    chore: add hexo log

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 'use strict';
 
-var api = require('./lib/api');
+const linkHolder = require('./lib/link-holder');
 
-hexo.extend.filter.register('before_post_render', api.process);
+const log = hexo.log;
 
-hexo.extend.filter.register('after_generate', api.verifyComplete);
+const ISSUE_LINK_REG = /(https:\/\/github.com\/[^/]+\/[^/]+\/issues\/\d+)/g;
+
+function processContent(data) {
+  linkHolder.add(data.issue_link, data.permalink);
+
+  data.content = data.content.replace(ISSUE_LINK_REG, function(url) {
+    // 如果含有指向当前页面所代表的 issues link，则不转换，因为这一般是保留链接，而不是引用s
+    if (url === data.issue_link) return url;
+
+    const link = linkHolder.get(url);
+    if (link) return link;
+
+    linkHolder.wait(url, (link) => {
+      data.content = data.content.replace(url, link);
+    });
+
+    return url;
+  });
+}
+
+function process(data) {
+  // 只对本地文章处理
+  if (data.layout === 'post') {
+    processContent(data);
+  }
+}
+
+function verifyComplete() {
+  var urls = linkHolder.getWatchingNames();
+  if (urls.length) {
+    log.e('匹配不到下列 URL：' + urls.join(', '));
+  }
+}
+
+hexo.extend.filter.register('before_post_render', process);
+
+hexo.extend.filter.register('after_generate', verifyComplete);
